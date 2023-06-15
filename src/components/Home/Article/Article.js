262aee4163e5fd78773ebe88e75d3be813ea49d0
Kevin Mars <kevin.eth.420@gmail.com>
2023-06-15 10:35:09 +0800

    article section completed

--- src/components/Home/Article/Article.js
+++ src/components/Home/Article/Article.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import './article.css'
 
+import ArticleContent from './ArticleContent'
+
 const Article = () => {
     return (
         <section className='article__section'>
-            <h1 className='equipment__title'>陶瓷貼片推薦文章</h1>
-            <p className='equipment__subtitle'>台中陶瓷貼片專門所 - 朗日牙藝為大家精選的牙齒美白推薦知識都在這裡哦</p>
+            <h1 className='article__title'>陶瓷貼片推薦文章</h1>
+            <p className='article__subtitle'>台中陶瓷貼片專門所 - 澄臻美學為大家精選的牙齒美白推薦知識都在這裡哦</p>
+            <div className='article__container'>
+                {ArticleContent.map((item, index) => {
+                    return (
+                        <a href='/' className='article__content__container' key={index}>
+                            <div className='article__thumbnail__container'>
+                                <img src={item.thumbnail} alt="thumbnail_1" className='article__thumbnail' />
+                            </div>
+                            <div className='article__content__title'>
+                                {item.title}
+                            </div>
+                            <p className='article__content__text'>
+                                {(item.content.length > 100) ? item.content.slice(0, 99) + '...' : item.title}
+                                <br />
+                                <a href="/" className='read__more__button'>繼續閱讀</a>
+                            </p>
+                        </a>
+                    )
+                })}
+            </div>
         </section>
     )
 }
