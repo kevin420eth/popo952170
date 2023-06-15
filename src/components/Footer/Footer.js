6795a866653d7c7d8cbb8c04efbc606c1eecdc59
Kevin Mars <kevin.eth.420@gmail.com>
2023-06-16 05:04:19 +0800

    footer completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import './footer.css'
+import logo from '../../assets/logo.png'
+import line_qrcode from '../../assets/line_qrcode.jpg'
 
 const Footer = () => {
+    const currentDate = new Date()
+    const currentYear = currentDate.getFullYear()
+
     return (
         <div className='footer__section'>
             <div className='footer__content__container'>
                 <div className='footer__logo__container'>
-                    Logo
+                    <img src={logo} alt="logo" className='footer__logo__image' />
                 </div>
                 <div className='footer__contact__information'>
-                    <p className='footer__telephone'>(04)-22355670</p>
-                    <p className='footer__address'>台中市北屯區文心路四段151號</p>
+                    <p className='footer__telephone'>電話: (04)-22355670</p>
+                    <p className='footer__address'>地址: 台中市北屯區文心路四段151號</p>
+                </div>
+                <div className='footer__qrcode__container'>
+                    <img src={line_qrcode} alt="logo" className='footer__qrcode__image' />
                 </div>
             </div>
-            <p className='footer__rights__reserved'>All rights reserved</p>
+            <hr />
+            <p className='footer__rights__reserved'>Copyright© {currentYear} 澄臻美學牙醫診所 版權所有</p>
         </div>
     )
 }
