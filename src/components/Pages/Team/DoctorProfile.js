1a708a3a665ace5eec96df79ad1424c7241b1a0c
Kevin Mars <kevin.eth.420@gmail.com>
2023-07-03 11:15:29 +0800

    alright then gun

--- src/components/Pages/Team/DoctorProfile.js
+++ src/components/Pages/Team/DoctorProfile.js
@@ -5,16 +5,177 @@ import doctor_3 from '../../../assets/doctor_3.png'
 import doctor_4 from '../../../assets/doctor_4.png'
 import doctor_5 from '../../../assets/doctor_5.png'
 import doctor_6 from '../../../assets/doctor_6.png'
+import doctor_7 from '../../../assets/doctor_7.png'
 
 const DoctorProfile = [
     {
-        name:"",
-        englishName:"",
-        title:'',
+        name:"林昌延",
+        englishName:"Johnny Lin",
+        title:'院長',
         profilePhoto:doctor_0,
-        education:[],
-        majorTreatment:[]
-    }
+        education:[
+            "中山醫學大學牙醫學士",
+            "前中山醫學大學附設醫院醫師",
+            "美國塔弗茲大學 牙科訓練研習",
+            "紐約佛拉迪米爾大學 人工植牙認證技師",
+            "倫敦劍橋大學 牙齒工程學碩士"
+        ],
+        majorTreatment:[
+            "牙周植牙專科",
+            "牙周病",
+            "幼兒齲齒",
+            "活動假牙",
+            "固定假牙",
+            "根管治療"
+        ]
+    },
+    {
+        name:"江逆鱗",
+        englishName:"Johnny Lin",
+        title:'副院長',
+        profilePhoto:doctor_1,
+        education:[
+            "中山醫學大學牙醫學士",
+            "前中山醫學大學附設醫院醫師",
+            "美國塔弗茲大學 牙科訓練研習",
+            "紐約佛拉迪米爾大學 人工植牙認證技師",
+            "倫敦劍橋大學 牙齒工程學碩士"
+        ],
+        majorTreatment:[
+            "美學植牙專科",
+            "牙周病",
+            "幼兒齲齒",
+            "活動假牙",
+            "固定假牙",
+            "根管治療"
+        ]
+    },
+    {
+        name:"林強尼",
+        englishName:"Johnny Lin",
+        title:'醫師',
+        profilePhoto:doctor_2,
+        education:[
+            "中山醫學大學牙醫學士",
+            "前中山醫學大學附設醫院醫師",
+            "美國塔弗茲大學 牙科訓練研習",
+            "紐約佛拉迪米爾大學 人工植牙認證技師",
+            "倫敦劍橋大學 牙齒工程學碩士"
+        ],
+        majorTreatment:[
+            "矯正專科",
+            "牙周病",
+            "幼兒齲齒",
+            "活動假牙",
+            "固定假牙",
+            "根管治療"
+        ]
+    },
+    {
+        name:"林安海",
+        englishName:"Johnny Lin",
+        title:'醫師',
+        profilePhoto:doctor_3,
+        education:[
+            "中山醫學大學牙醫學士",
+            "前中山醫學大學附設醫院醫師",
+            "美國塔弗茲大學 牙科訓練研習",
+            "紐約佛拉迪米爾大學 人工植牙認證技師",
+            "倫敦劍橋大學 牙齒工程學碩士"
+        ],
+        majorTreatment:[
+            "兒童專科",
+            "牙周病",
+            "幼兒齲齒",
+            "活動假牙",
+            "固定假牙",
+            "根管治療"
+        ]
+    },
+    {
+        name:"林波波",
+        englishName:"Johnny Lin",
+        title:'醫師',
+        profilePhoto:doctor_4,
+        education:[
+            "中山醫學大學牙醫學士",
+            "前中山醫學大學附設醫院醫師",
+            "美國塔弗茲大學 牙科訓練研習",
+            "紐約佛拉迪米爾大學 人工植牙認證技師",
+            "倫敦劍橋大學 牙齒工程學碩士"
+        ],
+        majorTreatment:[
+            "口外專科",
+            "牙周病",
+            "幼兒齲齒",
+            "活動假牙",
+            "固定假牙",
+            "根管治療"
+        ]
+    },
+    {
+        name:"林瑟薇",
+        englishName:"Johnny Lin",
+        title:'醫師',
+        profilePhoto:doctor_5,
+        education:[
+            "中山醫學大學牙醫學士",
+            "前中山醫學大學附設醫院醫師",
+            "美國塔弗茲大學 牙科訓練研習",
+            "紐約佛拉迪米爾大學 人工植牙認證技師",
+            "倫敦劍橋大學 牙齒工程學碩士"
+        ],
+        majorTreatment:[
+            "根管治療專科",
+            "牙周病",
+            "幼兒齲齒",
+            "活動假牙",
+            "固定假牙",
+            "根管治療"
+        ]
+    },
+    {
+        name:"林波奇",
+        englishName:"Johnny Lin",
+        title:'醫師',
+        profilePhoto:doctor_6,
+        education:[
+            "中山醫學大學牙醫學士",
+            "前中山醫學大學附設醫院醫師",
+            "美國塔弗茲大學 牙科訓練研習",
+            "紐約佛拉迪米爾大學 人工植牙認證技師",
+            "倫敦劍橋大學 牙齒工程學碩士"
+        ],
+        majorTreatment:[
+            "家庭牙醫專科",
+            "牙周病",
+            "幼兒齲齒",
+            "活動假牙",
+            "固定假牙",
+            "根管治療"
+        ]
+    },
+    {
+        name:"林森林",
+        englishName:"Johnny Lin",
+        title:'醫師',
+        profilePhoto:doctor_7,
+        education:[
+            "中山醫學大學牙醫學士",
+            "前中山醫學大學附設醫院醫師",
+            "美國塔弗茲大學 牙科訓練研習",
+            "紐約佛拉迪米爾大學 人工植牙認證技師",
+            "倫敦劍橋大學 牙齒工程學碩士"
+        ],
+        majorTreatment:[
+            "矯正專科",
+            "牙周病",
+            "幼兒齲齒",
+            "活動假牙",
+            "固定假牙",
+            "根管治療"
+        ]
+    },
 ]
 
 export default DoctorProfile
--- src/components/Pages/Team/Team.js
+++ src/components/Pages/Team/Team.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import './team.css'
+
 import Navbar from '../../Header/Navbar/Navbar'
+import DoctorProfile from './DoctorProfile'
 
 import banner from '../../../assets/banner.png'
-
-import doctor_0 from '../../../assets/doctor_0.png'
-import doctor_1 from '../../../assets/doctor_1.png'
-import doctor_2 from '../../../assets/doctor_2.png'
-import doctor_3 from '../../../assets/doctor_3.png'
-import doctor_4 from '../../../assets/doctor_4.png'
-import doctor_5 from '../../../assets/doctor_5.png'
-import doctor_6 from '../../../assets/doctor_6.png'
-import doctor_7 from '../../../assets/doctor_7.png'
 
 const Team = () => {
     return (
@@ -25,62 +18,17 @@ const Team = () => {
             <section className='team__section'>
                 <p className='team__section__title'>醫療團隊</p>
                 <div className='team__profile__section'>
-                    <a href='/team/johnnylin' className='doctor__profile__preview'>
-                        <div className='doctor__photo__preview'>
-                            <img src={doctor_0} alt="" />
-                        </div>
-                        <p className='doctor__name'>林昌延 院長</p>
-                        <p className='major__skill'>牙周植牙專科</p>
-                    </a>
-                    <a href='/team/johnnylin' className='doctor__profile__preview'>
-                        <div className='doctor__photo__preview'>
-                            <img src={doctor_1} alt="" />
-                        </div>
-                        <p className='doctor__name'>江逆鱗 副院長</p>
-                        <p className='major__skill'>美學植牙專科</p>
-                    </a>
-                    <a href='/team/linjohnny' className='doctor__profile__preview'>
-                        <div className='doctor__photo__preview'>
-                            <img src={doctor_2} alt="" />
-                        </div>
-                        <p className='doctor__name'>林強尼 醫師</p>
-                        <p className='major__skill'>矯正專科</p>
-                    </a>
-                    <a href='/team/linanhai' className='doctor__profile__preview'>
-                        <div className='doctor__photo__preview'>
-                            <img src={doctor_3} alt="" />
-                        </div>
-                        <p className='doctor__name'>林安海 醫師</p>
-                        <p className='major__skill'>兒童專科</p>
-                    </a>
-                    <a href='/team/linpopo' className='doctor__profile__preview'>
-                        <div className='doctor__photo__preview'>
-                            <img src={doctor_4} alt="" />
-                        </div>
-                        <p className='doctor__name'>林波波 醫師</p>
-                        <p className='major__skill'>口外專科</p>
-                    </a>
-                    <a href='/team/linhathway' className='doctor__profile__preview'>
-                        <div className='doctor__photo__preview'>
-                            <img src={doctor_5} alt="" />
-                        </div>
-                        <p className='doctor__name'>林瑟薇 醫師</p>
-                        <p className='major__skill'>根管治療專科</p>
-                    </a>
-                    <a href='/team/linpoki' className='doctor__profile__preview'>
-                        <div className='doctor__photo__preview'>
-                            <img src={doctor_6} alt="" />
-                        </div>
-                        <p className='doctor__name'>林波奇 醫師</p>
-                        <p className='major__skill'>家庭牙醫專科</p>
-                    </a>
-                    <a href='linforest' className='doctor__profile__preview'>
-                        <div className='doctor__photo__preview'>
-                            <img src={doctor_7} alt="" />
-                        </div>
-                        <p className='doctor__name'>林森林 醫師</p>
-                        <p className='major__skill'>矯正專科</p>
-                    </a>
+                    {DoctorProfile.map((item) => {
+                        return (
+                            <a href='/team/johnnylin' className='doctor__profile__preview'>
+                                <div className='doctor__photo__preview'>
+                                    <img src={item.profilePhoto} alt="" />
+                                </div>
+                                <p className='doctor__name'>{item.name}</p>
+                                <p className='major__skill'>{item.majorTreatment[0]}</p>
+                            </a>
+                        )
+                    })}
                 </div>
             </section>
         </div>
