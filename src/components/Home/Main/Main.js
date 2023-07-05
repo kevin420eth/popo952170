894091a8973e2bb1410b6fee23460ce8748e4bde
Kevin Mars <kevin.eth.420@gmail.com>
2023-07-06 03:26:28 +0800

    normal modified

--- src/components/Home/Main/Main.js
+++ src/components/Home/Main/Main.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './main.css'
-import home_logo from '../../../assets/logo.png'
+import video from '../../../assets/video.mp4'
 
 const Main = () => {
   return (
     <section className='home__section'>
-      <img src={home_logo} alt="" className='home__logo'/>
-      <p className='home__title'>匠心之道載彩繪，精雕細琢石成金</p>
-      <p className='home__description'>完美的笑容，從澄臻美學開始</p>
-      
+      <div className='video__container'>
+        <video src={video} width='500' height='300' autoplay='true' loop muted></video>
+      </div>
+
     </section>
   )
 }
