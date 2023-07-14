2b6dcbb87540f723bcb0cb52f3197fc5cd57d375
Kevin Mars <kevin.eth.420@gmail.com>
2023-07-15 02:08:39 +0800

    add cases page

--- src/components/Pages/Cases/Cases.js
+++ src/components/Pages/Cases/Cases.js
@@ -2,6 +2,11 @@ import React from 'react'
 import banner from '../../../assets/banner.png'
 import Header from '../../Header/Header'
 
+import './cases.css'
+
+import case_0 from '../../../assets/cases_0.png'
+import case_10 from '../../../assets/case_10.png'
+
 const Cases = () => {
   return (
     <div>
@@ -11,6 +16,24 @@ const Cases = () => {
       <div className='banner__container'>
         <img src={banner} alt="" />
       </div>
+      <section className='cases__section'>
+        <p >案例回顧</p>
+        <div className='cases__showcase__container'>
+          <div className='case__showcase'>
+            <img src={case_10} alt="" className='case__showcase__image' />
+          </div>
+          <div className='case__showcase'>
+            <img src={case_0} alt="" className='case__showcase__image' />
+          </div>
+          <div className='case__showcase'>
+            <img src={case_0} alt="" className='case__showcase__image' />
+          </div>
+          <div className='case__showcase'>
+            <img src={case_0} alt="" className='case__showcase__image' />
+          </div>
+        </div>
+
+      </section>
     </div>
   )
 }
