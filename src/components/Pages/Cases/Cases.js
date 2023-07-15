b02236b154550ef4d07ee35da7b2c04704d5d436
Kevin Mars <kevin.eth.420@gmail.com>
2023-07-16 04:28:47 +0800

    cases page completed 50%

--- src/components/Pages/Cases/Cases.js
+++ src/components/Pages/Cases/Cases.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import banner from '../../../assets/banner.png'
 import Header from '../../Header/Header'
 
 import './cases.css'
 
-import case_0 from '../../../assets/cases_0.png'
+import case_0 from '../../../assets/case_0.png'
+import case_1 from '../../../assets/case_1.png'
+import case_2 from '../../../assets/case_2.png'
+import case_3 from '../../../assets/case_3.png'
+import case_4 from '../../../assets/case_4.png'
+import case_5 from '../../../assets/case_5.png'
 import case_10 from '../../../assets/case_10.png'
 
 const Cases = () => {
@@ -13,29 +17,34 @@ const Cases = () => {
       <header>
         <Header />
       </header>
-      <div className='banner__container'>
-        <img src={banner} alt="" />
-      </div>
-      <section className='cases__section section'>
-        <p >案例回顧</p>
-        <div className='cases__showcase__container'>
-          <div className='case__showcase'>
-            <img src={case_10} alt="" className='case__showcase__image' />
-          </div>
-          <div className='case__showcase'>
-            <img src={case_0} alt="" className='case__showcase__image' />
-          </div>
-          <div className='case__showcase'>
-            <img src={case_0} alt="" className='case__showcase__image' />
-          </div>
-          <div className='case__showcase'>
-            <img src={case_0} alt="" className='case__showcase__image' />
+      <section className='cases__main__section section'>
+        <div className='spotlight__container'>
+          <a href="https:google.com" className='main__spotlight__container shadow'>
+            <img src={case_10} alt="" className='main__spotlight__imgae' />
+          </a>
+          <div className='sub__spotlight__container'>
+            <a href="https:google.com" className='sub__spotlight__flagment shadow'>
+              <img src={case_4} alt="" className='sub__spotlight__flagment__image' />
+            </a>
+            <a href="https:google.com" className='sub__spotlight__flagment'>
+              <img src={case_5} alt="" className='sub__spotlight__flagment__image' />
+            </a>
           </div>
         </div>
-
+      </section>
+      <section className='cases__section section'>
+        <a href="https:google.com" className='model__container shadow'>
+          <img src={case_3} alt="" className='model__image' />
+        </a>
+        <a href="https:google.com" className='model__container shadow'>
+          <img src={case_1} alt="" className='model__image' />
+        </a>
+        <a href="https:google.com" className='model__container shadow'>
+          <img src={case_2} alt="" className='model__image' />
+        </a>
       </section>
     </div>
-      )
-    }
+  )
+}
 
 export default Cases
