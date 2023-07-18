f8b4a26b0d0765ff996fd0246d77ff816909bdeb
Kevin Mars <kevin.eth.420@gmail.com>
2023-07-19 07:11:34 +0800

    home appointment section modified

--- src/components/Home/Appointment/Appointment.js
+++ src/components/Home/Appointment/Appointment.js
@@ -1,19 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './appointment.css'
 // import image from '../../../assets/appointment_image.png'
 
 const Appointment = () => {
     return (
-        <section className='appointment__section section'>
-            <div className='image__container'>
-                {/* <img src={image} alt="appointment" /> */}
+        <section className='home__appointment__section section'>
+            <div className='home__appointment__container'>
+                <div className='home__image__container'>
+                    {/* <img src={image} alt="appointment" /> */}
+                </div>
+                <h1 className='home__appointment__title'>動人笑容，總源自於雙眸綻放的神韻</h1>
+                <p className='home__appointment__description'>
+                    笑容設計就像一門藝術創作，專注於「牙科美學」的我們堅信著。
+                    <br />醫者們近乎苛求的美學設計工藝，只為了讓您的笑容更迷人、更自然。
+                </p>
+                <Link to='/contact' className='home__appointment__button'>
+                    立即預約諮詢
+                </Link>
             </div>
-            <h1 className='appointment__title'>動人笑容，總源自於雙眸綻放的神韻</h1>
-            <p className='appointment__subtitle'>Every touching smile, start from the eyes.</p>
-            <p className='appointment__description'>
-                笑容設計就像一門藝術創作，專注於「牙科美學」的我們堅信著。<br />醫者們近乎苛求的美學設計工藝，只為了讓您的笑容更迷人、更自然
-            </p>
-            <a href="/" className='button appointment__button'>立即預約諮詢</a>
         </section>
     )
 }
