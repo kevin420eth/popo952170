f63f51f93b575c08b90cb247baca14dc6bdf65ca
Kevin Mars <kevin.eth.420@gmail.com>
2023-07-19 14:22:58 +0800

    home appointment section modified

--- src/components/Home/Appointment/Appointment.js
+++ src/components/Home/Appointment/Appointment.js
@@ -12,8 +12,8 @@ const Appointment = () => {
                 </div>
                 <h1 className='home__appointment__title'>動人笑容，總源自於雙眸綻放的神韻</h1>
                 <p className='home__appointment__description'>
-                    笑容設計就像一門藝術創作，專注於「牙科美學」的我們堅信著。
-                    <br />醫者們近乎苛求的美學設計工藝，只為了讓您的笑容更迷人、更自然。
+                    微笑的魅力如同一幅藝術傑作，笑容設計更被視為藝術創作的絕佳體現
+                    <br />我們致力於追求完美的美學設計工藝，為你打造一個迷人且獨一無二的笑容。
                 </p>
                 <Link to='/contact' className='home__appointment__button'>
                     立即預約諮詢
